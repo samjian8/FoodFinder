500bbef1ea68c2c0cec6da7e5c890a89ee28e55a
samjian8 <samjian6289@gmail.com>
2025-09-12 22:33:10 -0400

    Initial data fetch

--- fetcher.py
+++ fetcher.py
@@ -1,1 +1,14 @@
+import os
+import dotenv
+import requests
 
+dotenv.load_dotenv()
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+
+lat = '43.6452'
+lon = '-79.3806'
+radius = 1000 # in meters
+
+url = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={lat},{lon}&radius=1000&type=restaurant&key={GOOGLE_API_KEY}"
+response = requests.get(url=url)
+print(response.json())
